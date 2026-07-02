b2d0f47d1cb703baa11e08561178effab0304b9f
Joshua Ange <joshange@gmail.com>
2026-07-02 23:56:49 +0200

    Comment out AJAX load line in script.js
    Commented out the AJAX load line for content.

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
     // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
     // var region = location.hash.toString() || '#first';
-    $('#content').load(region.slice(1) + '.html')
+    // $('#content').load(region.slice(1) + '.html')
     
   });
   
