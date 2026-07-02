9fde9ea1e36bf1fb8815ce99d1b6b7530169ed7f
Joshua Ange <joshange@gmail.com>
2026-07-03 00:04:06 +0200

    Fix comment formatting for region variable assignment

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 
     // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
+    var region = location.hash.toString() || '#first';
     $('#content').load(region.slice(1) + '.html')
     
   });
